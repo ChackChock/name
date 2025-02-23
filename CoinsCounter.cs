0abe457fb214c77346fd52b06a87ddf182dbe843
Burkin Nikita <92577296+ChackChock@users.noreply.github.com>
2025-02-23 13:49:23 +0300

    Add files via upload

--- CoinsCounter.cs
+++ CoinsCounter.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
+ 
 public class CoinsCounter : MonoBehaviour
 {
-    private int coins;
-
+    //Число собранных монет
+    public int coins;
+ 
+    //Метод, увеличивающий число монеток
     public void CollectCoins()
     {
         coins++;
-        print("Собранные монетки: " + coins);
     }
 }
--- Health.cs
+++ Health.cs
@@ -2,20 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+ 
 public class Health : MonoBehaviour
 {
-    private int health = 10;
-    private int maxHealth = 10;
-
+    //Текущее здоровье игрока
+    public int currenthealth = 10;
+    //Максимальное здоровье игрока
+    public int maxHealth = 10;
+    //Компонент, отвечающий за проигрывание звуков
+    public AudioSource audioSource;
+    //Звуковой файл, содержащий звуковой эффект нанесения урона
+    public AudioClip damageSound;
+    //Метод, обрабатывающий нанесённый урон
     public void TakeDamage(int damage)
     {
-        health -= damage;
-
-        if(health <= 0)
+        currenthealth -= damage;
+ 
+        //Если здоровье ещё есть, то проигрывается звук нанесения урона
+        if (currenthealth > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        //Если здоровья нет, то перезапускается текущая сцена
+        else
         {
             int sceneIndex = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(sceneIndex);
         }
     }
 }
+ 
